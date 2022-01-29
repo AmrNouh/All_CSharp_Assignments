8d83adcae75bdec9ed9194e9bf9aad7bbf0a419d
Amr M. Nouh <AMR.MAMDOUH1371@ci.menofia.edu.eg>
2022-01-29 03:21:03 +0200

    work on Final Exam

--- Day06/ExaminationSystem/FinalExam.cs
+++ Day06/ExaminationSystem/FinalExam.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ExaminationSystem
 {
-    internal class FinalExam
+    internal class FinalExam : Exam
     {
+        public FinalExam() : base() { }
+        public FinalExam(TimeSpan examTime, Subject subjectInfo, Student studentInfo, int numberOfQuestion, QuestionsList questions, AnswersList answers) : base(examTime, subjectInfo, studentInfo, numberOfQuestion, questions, answers) { }
+
+        public override double Correct(QuestionsList examQuestions, AnswersList correctAnswers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PrintExam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ShowExam()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
